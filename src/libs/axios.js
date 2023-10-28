08ed34fe0bd109fc10a0698940d63bf2c9c09c1c
Zul Fahmi <Zul@WOWBIDs-MacBook-Pro.local>
2023-10-28 21:03:54 +0700

    put sensitive data to .env

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -1,8 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
-const baseUrl = 'https://api.themoviedb.org/3';
-const apiKey = 'd9412cedb0c4f57eb84d26c5d40ed00b';
-export const imageUrl = 'https://image.tmdb.org/t/p/original'
+const token = process.env.NEXT_PUBLIC_AUTHORIZATION_TOKEN;
+const baseUrl = process.env.NEXT_PUBLIC_API_URL;
+const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+export const imageUrl = process.env.NEXT_PUBLIC_IMG_URL;
 
 export const axiosClient = axios.create({
   baseURL: baseUrl,
@@ -10,8 +11,7 @@ export const axiosClient = axios.create({
     api_key: apiKey,
   },
   headers: {
-    'Content-Type': 'application/json',
-    Authorization:
-      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkOTQxMmNlZGIwYzRmNTdlYjg0ZDI2YzVkNDBlZDAwYiIsInN1YiI6IjY1MmU4MzU0YTgwMjM2MDExYWM3N2FlYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TK05lqqxrZ7ry5BQ7635nvXltGeNPKpZlD90cfVMeC8',
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${token}`,
   },
 });
